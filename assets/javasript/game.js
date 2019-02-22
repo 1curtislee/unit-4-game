bbb2d10dfb590b0582eaea4c0c00e72125bc3e5c
1curtislee <1curtislee@gmail.com>
2019-02-22 15:53:23 -0800

    all game features working

--- assets/javasript/game.js
+++ assets/javasript/game.js
@@ -16,14 +16,14 @@ $("#wins").text("Wins: " + wins);
 $("#losses").text("Losses: " + losses);
 
 function reset() {
-  var random = Math.floor(Math.random()*101+19)
+  random = Math.floor(Math.random()*101+19)
   $("#wizNumber").text(random);
 
-  var cr1 = Math.floor(Math.random()*11+1);
-  var cr2 = Math.floor(Math.random()*11+1);
-  var cr3 = Math.floor(Math.random()*11+1);
-  var cr4 = Math.floor(Math.random()*11+1);
-  var playerTotal = 0;
+  cr1 = Math.floor(Math.random()*11+1);
+  cr2 = Math.floor(Math.random()*11+1);
+  cr3 = Math.floor(Math.random()*11+1);
+  cr4 = Math.floor(Math.random()*11+1);
+  playerTotal = 0;
   $("#crSum").text(playerTotal);
 }
 
@@ -31,34 +31,47 @@ function reset() {
 function winner() {
   alert ("You Won!");
   wins++
-  $("#wins").text(wins);
+  $("#wins").text("Wins: " + wins);
   reset();
 }
 function loser() {
   alert ("BUST!");
   losses++
-  $("#losses").text(losses);
+  $("#losses").text("Losses: " + losses);
+  reset();
 }
 
 //crystals
 $("#cr1").on('click', function(){
   playerTotal = playerTotal + cr1;
   $('#crSum').text(playerTotal);
+  end();
 })
 $("#cr2").on('click', function(){
   playerTotal = playerTotal + cr2;
   $('#crSum').text(playerTotal);
+  end();
 })
 $("#cr3").on('click', function(){
   playerTotal = playerTotal + cr3;
   $('#crSum').text(playerTotal);
+  end();
 })
 $("#cr4").on('click', function(){
   playerTotal = playerTotal + cr4;
   $('#crSum').text(playerTotal);
+  end();
 })
 
-
+//win/loss:
+function end() {
+  console.log("did you win yet?");
+  if (playerTotal == random) {
+    winner();
+  } else if (playerTotal > random) {
+    loser();
+  } else {null}
+}
 });
 
 
